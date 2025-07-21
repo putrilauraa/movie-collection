d3767392720e8e259930838a717e4654b4dd589d
putrilauraa <rahmpautrilaura@gmail.com>
2025-07-21 21:14:47 +0700

    add rules

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "react/jsx-no-undef": "off",
+        "@next/next/no-img-element": "off",
+    },
+};
